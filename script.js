201a5ff5b936b154c358ebd2f61242bc7eb17002
RVEPRTY <n3xtt1m3off@gmail.com>
2026-01-17 18:33:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  document.getElementById('contact-msg').textContent = "Thank you! Your message has been received.";
+  this.reset();
+});
