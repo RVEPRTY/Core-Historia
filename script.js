4ab59058653dc1e0b6f7c629764a4e92e9c45317
RVEPRTY <n3xtt1m3off@gmail.com>
2026-01-22 07:41:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loading-screen");
+  if (loader) {
+    loader.style.display = "none";
+  }
+});
+
 const SITE_ONLINE = true; 
 
 if (!SITE_ONLINE) {
